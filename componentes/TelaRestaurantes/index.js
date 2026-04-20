d91560c1ad35919c5e5952d143a674732a73299b
Flávio Félix <fau_felix30@hotmail.com>
2026-04-20 15:36:01 -0300

    Feito as telas inicial e restaurantes

--- componentes/TelaRestaurantes/index.js
+++ componentes/TelaRestaurantes/index.js
@@ -1,9 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+
+import Img1 from '../../assets/restaurante01.png';
+import Img2 from '../../assets/restaurante02.png';
+import Img3 from '../../assets/restaurante03.png';
+import estilos from "./estilos";
 
 export default function TelaRestaurantes() {
   return (
-    <View>
-      <Text>Tela Restaurantes</Text>
+    <View style={estilos.container}>
+      <Text style={estilos.titulo}>Bares e Restaurantes</Text>
+      <Text style={estilos.descricao}>O prazer da boa comida você encontra em Paraty. Com inúmeras opções de restaurantes, a cidade oferece o melhor da culinária internacional, juntamente à saborosa cozinha caiçara. Confira alguns bares e restaurantes famosos.</Text>
+
+      <View style={estilos.card}>
+        <Image source={Img1} style={estilos.cardImg} />
+        <Text style={estilos.cardTitulo}>Armazén Mar</Text>
+        <Text>Localização: Rod Rio-Santos</Text>
+      </View>
+      <View style={estilos.card}>
+        <Image source={Img2} style={estilos.cardImg} />
+        <Text style={estilos.cardTitulo}>Bendita's Restaurante</Text>
+        <Text>Localização: Centro Histórico</Text>
+      </View>
+      <View style={estilos.card}>
+        <Image source={Img3} style={estilos.cardImg} />
+        <Text style={estilos.cardTitulo}>SEREIA DO MAR PIZZA-BAR</Text>
+        <Text>Localização: Praia do Jabaquara</Text>
+      </View>
     </View>
   );
 }
